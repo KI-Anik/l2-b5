394738a7fb895d925182ed0a2c5936623e8b745d
KI-Anik <iamkhairul101@gmail.com>
2025-06-29 11:58:46 +0600

    added date picker and decorate pick option form

--- Mission-4-redux/m22-23-todo-app/src/components/module/tasks/AddTaskModal.tsx
+++ Mission-4-redux/m22-23-todo-app/src/components/module/tasks/AddTaskModal.tsx
@@ -15,10 +15,26 @@ import {
   FormItem,
   FormLabel,
 } from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Description } from "@radix-ui/react-dialog";
 import { useForm } from "react-hook-form";
+import { Calendar } from "@/components/ui/calendar";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
 
 export function AddTaskModal() {
   const form = useForm();
@@ -29,55 +45,124 @@ export function AddTaskModal() {
 
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button>Add Task</Button>
-        </DialogTrigger>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
 
-        <DialogContent className="sm:max-w-[425px]">
-          <Description className="sr-only">this className is = only visible to blind people </Description>
+      <DialogContent className="sm:max-w-[425px]">
+        <Description className="sr-only">
+          this className is = only visible to blind people{" "}
+        </Description>
 
-          <DialogHeader>
-            <DialogTitle>Fill the form</DialogTitle>
-          </DialogHeader>
+        <DialogHeader>
+          <DialogTitle>Fill the form</DialogTitle>
+        </DialogHeader>
 
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Form {...form}>
+          <form className="space-y-5" onSubmit={form.handleSubmit(onSubmit)}>
+            {/* form input start */}
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input {...field} value={field.value || ""}></Input>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="Desciption"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea {...field} value={field.value || ""}></Textarea>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            {/* form input end */}
+
+            {/*  pick a option start */}
+             <FormField
+          control={form.control}
+          name="Priority"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Priority</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a value" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="low">low</SelectItem>
+                  <SelectItem value="medium">medium</SelectItem>
+                  <SelectItem value="high">high</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+            {/* pick a option end */}
+
+            {/* calendar start */}
               <FormField
-                control={form.control}
-                name="title"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Title</FormLabel>
-                    <FormControl>
-                      <Input {...field} value={field.value || ""}></Input>
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="Desciption"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Description</FormLabel>
-                    <FormControl>
-                      <Textarea {...field} value={field.value || ""}></Textarea>
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+          control={form.control}
+          name="dueDate"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Due Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        " pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    // disabled={(date) =>
+                    //   date > new Date() || date < new Date("1900-01-01")
+                    // }
+                    captionLayout="dropdown"
+                  />
+                </PopoverContent>
+              </Popover>
+            </FormItem>
+          )}
+        />
+            {/* calendar end */}
 
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button variant="outline">Cancel</Button>
-                </DialogClose>
-                <Button type="submit">Save changes</Button>
-              </DialogFooter>
-            </form>
-          </Form>
-        </DialogContent>
-      </form>
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
     </Dialog>
   );
 }
